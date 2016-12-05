035150cebea5bf351f7a0baaf5ac116342ca4f5d
Matthew <mrzchyen@gmail.com>
2016-12-05 15:01:39 +0100

    Update UsersTableSeeder.php

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,9 +12,9 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
       DB::table('users')->insert([
-        'name' => 'chyen',
-        'email' => 'mrzchyen@gmail.com',
-        'password' => bcrypt('cokolwiek'),
+        'name' => 'admin',
+        'email' => 'admin@admin.com',
+        'password' => bcrypt('secret'),
       ]);
     }
 }
